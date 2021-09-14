c1d59c5d7321a195a1e6eff67d4b880905c22561
Ades-Angel <64943306+Ades-Angel@users.noreply.github.com>
2021-09-14 22:06:10 +0200

    Update woohoo.java

--- Exercise/Set 1/woohoo.java
+++ Exercise/Set 1/woohoo.java
@@ -1,3 +1,9 @@
+/*
+Chloe Antonozzi
+03/09/2021
+
+First java program
+*/
 class WooHoo {
     public static void main (String[] args) {
         System.out.println("I worte my first Java program! WooHoo!");
