ba55c7092c51f398d48efba0aa52e63ad8f61fb6
Ades-Angel <64943306+Ades-Angel@users.noreply.github.com>
2021-09-23 21:35:35 +0200

    Update HelloWorld.java

--- Exercise/Set 1/HelloWorld.java
+++ Exercise/Set 1/HelloWorld.java
@@ -6,18 +6,19 @@ Example to demonstrate console output
 */
 
 public class HelloWorld {
-    //put message on console
+    // put message on console
     void show() {
         System.out.println("Hello world, this is a Java program.");
         System.out.println(" ");
         System.out.println("");
         System.out.println("Good luck, programmer");
     }
-    public static void main(String[]args) {
+
+    public static void main(String[] args) {
         (new HelloWorld()).show();
     }
 }
 
 // if display is chnaged in both line 10 and 15, then program works fine
-//Main and system are not know to java, not one of its functions
+// Main and system are not know to java, not one of its functions
 // A syntax error occurs
