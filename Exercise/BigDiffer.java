414c615e75dc36addda5e7e8973606c680b82258
Ades-Angel <64943306+Ades-Angel@users.noreply.github.com>
2021-09-23 21:31:33 +0200

    Add files via upload

---
+++ Exercise/BigDiffer.java
@@ -0,0 +1,61 @@
+
+/*
+Chloe Anotnozzi
+23/09/2021
+*/
+import java.util.*;
+
+public class BigDiffer {
+    Scanner scan = new Scanner(System.in);
+    int bigDiff;
+    int[] nums;
+
+    void readArray() {
+        int n;
+
+        System.out.println("How many values?");
+        n = scan.nextInt();
+        nums = new int[n];
+
+        System.out.println("Enter values");
+        /** i numbers from input read and stored in nums[0..i-1] is */
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = scan.nextInt();
+        }
+    }
+
+    void calculateBigDiff() {
+        int max;
+        int min;
+
+        max = nums[0]; // guaranteed to exits
+        min = nums[0];
+        /**
+         * max = max(nums[0....i-1]) && min = min(nums[0...i-1])
+         */
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > max) {
+                max = nums[i];
+            } else if (nums[i] < min) {
+                min = nums[i];
+            }
+        }
+        bigDiff = max - min;
+    }
+
+    void printResult() {
+        System.out.println("The biggest difference in these values is: ");
+        System.out.println(bigDiff);
+    }
+
+    void readAndGiveBigDiff() {
+        readArray();
+        calculateBigDiff();
+        printResult();
+
+    }
+
+    public static void main(String[] args) {
+        (new BigDiffer()).readAndGiveBigDiff();
+    }
+}
