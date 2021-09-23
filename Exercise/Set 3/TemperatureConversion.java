206696819ccb0928c1660ca7187785c41ff4f19b
Ades-Angel <64943306+Ades-Angel@users.noreply.github.com>
2021-09-23 21:39:27 +0200

    Update TemperatureConversion.java

--- Exercise/Set 3/TemperatureConversion.java
+++ Exercise/Set 3/TemperatureConversion.java
@@ -1,3 +1,4 @@
+
 /*
 Chloe Antonozzi
 16/09/2021
@@ -9,28 +10,29 @@ import java.util.Scanner;
 public class TemperatureConversion {
     Scanner scan = new Scanner(System.in);
 
-    public void conversion(){
+    public void conversion() {
         System.out.println("Give a temperature in Celsius or Fahrenheit (type q to Quit):");
-        while(scan.hasNextDouble()){
+        while (scan.hasNextDouble()) {
             double input = scan.nextDouble();
             char celsiusOrFahrenheit = scan.next().charAt(0);
 
-            if (celsiusOrFahrenheit == 'C' || celsiusOrFahrenheit == 'c'){
-                double temperature = (input*1.8) + 32;
-                System.out.println(input+" degrees Celsius = "+ temperature + " degrees Fahrenheit");
+            if (celsiusOrFahrenheit == 'C' || celsiusOrFahrenheit == 'c') {
+                double temperature = (input * 1.8) + 32;
+                System.out.println(input + " degrees Celsius = " + temperature + " degrees Fahrenheit");
                 System.out.println("");
                 System.out.println("Give a temperature in Celsius or Fahrenheit (type q to Quit):");
-            }else if (celsiusOrFahrenheit == 'F' || celsiusOrFahrenheit == 'f'){
-                double temperature = (input - 32)/1.8;
-                System.out.println(input+" degrees Fahrenheit = "+ temperature + " degrees Celsius");
+            } else if (celsiusOrFahrenheit == 'F' || celsiusOrFahrenheit == 'f') {
+                double temperature = (input - 32) / 1.8;
+                System.out.println(input + " degrees Fahrenheit = " + temperature + " degrees Celsius");
                 System.out.println("");
                 System.out.println("Give a temperature in Celsius or Fahrenheit (type q to Quit):");
-            }else{
+            } else {
                 System.out.println("love");
             }
         }
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         (new TemperatureConversion()).conversion();
     }
 }
