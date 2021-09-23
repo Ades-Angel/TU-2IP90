8d5c0622bd1c6ea7f7d3f69185bacae1c8c55368
Ades-Angel <64943306+Ades-Angel@users.noreply.github.com>
2021-09-23 21:38:01 +0200

    Update InterestWithChoice.java

--- Exercise/Set 2/InterestWithChoice.java
+++ Exercise/Set 2/InterestWithChoice.java
@@ -1,3 +1,4 @@
+
 /*
 Chloe Antonozzi
 14/09/2021
@@ -8,8 +9,8 @@ import java.util.Scanner;
 
 public class InterestWithChoice {
     Scanner scan = new Scanner(System.in);
-    
-    public void calculateInterest(){
+
+    public void calculateInterest() {
         System.out.println("Enter the starting amount: ");
         double balance = scan.nextDouble();
         double rate = 2.5;
@@ -17,18 +18,19 @@ public class InterestWithChoice {
         double deduction = -0.5;
         double interest;
 
-        if (balance >= 10000){
-            interest = (balance*rateBonus/100.0);
-            System.out.println(interest+balance);
-        }else if(balance>0 && balance<10000){
-            interest = (balance*rate/100.0);
-            System.out.println(interest+balance);
-        }else{
-            interest = (balance*deduction/100.0);
-            System.out.println(balance-interest);
+        if (balance >= 10000) {
+            interest = (balance * rateBonus / 100.0);
+            System.out.println(interest + balance);
+        } else if (balance > 0 && balance < 10000) {
+            interest = (balance * rate / 100.0);
+            System.out.println(interest + balance);
+        } else {
+            interest = (balance * deduction / 100.0);
+            System.out.println(balance - interest);
         }
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         (new InterestWithChoice()).calculateInterest();
     }
 }
