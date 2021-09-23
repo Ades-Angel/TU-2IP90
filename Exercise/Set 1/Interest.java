33d48e9c244878c6f89ee608fd62eab5b6e7fe30
Ades-Angel <64943306+Ades-Angel@users.noreply.github.com>
2021-09-23 21:35:56 +0200

    Update Interest.java

--- Exercise/Set 1/Interest.java
+++ Exercise/Set 1/Interest.java
@@ -1,3 +1,4 @@
+
 /*
 Chloe Antonozzi
 09/09/2021
@@ -9,29 +10,28 @@ import java.util.Scanner;
 public class Interest {
     Scanner scan = new Scanner(System.in);
 
-    public void calcInterest(){
+    public void calcInterest() {
         double interest;
         double tax;
 
         System.out.println("Enter the starting amount ");
         double balance = scan.nextDouble();
         System.out.println("Give interest rate as a percentage ");
-        double rate = scan.nextDouble();//percentage
+        double rate = scan.nextDouble();// percentage
         System.out.println("Give the tax rate as a percentage ");
         double taxRate = scan.nextDouble(); // percentage
 
-        interest = (balance*rate/100.0);
-        tax = ((balance/taxRate)/100.0);
+        interest = (balance * rate / 100.0);
+        tax = ((balance / taxRate) / 100.0);
 
-        System.out.println("After a year you get as interest: "+
-                            interest);
+        System.out.println("After a year you get as interest: " + interest);
 
-        System.out.println("After a year your balance with interest is: "+
-                            (balance+interest));
+        System.out.println("After a year your balance with interest is: " + (balance + interest));
 
-        System.out.println("This is the tax fo this year: "+tax);
+        System.out.println("This is the tax fo this year: " + tax);
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         (new Interest()).calcInterest();
     }
 }
