46baf7a4fd7fe89d47ffc1088b8d121bdfcf6b3c
Ades-Angel <64943306+Ades-Angel@users.noreply.github.com>
2021-09-23 21:39:07 +0200

    Update Summation.java

--- Exercise/Set 3/Summation.java
+++ Exercise/Set 3/Summation.java
@@ -1,3 +1,4 @@
+
 /*
 Chloe Antonozzi
 14/09/2021
@@ -12,23 +13,24 @@ public class Summation {
     int multiplication; // running sum
     int times; // number of numbers entered
 
-    void doSummation(){
+    void doSummation() {
         times = 0;
         multiplication = 1;
         System.out.println("Type a sequence of integer numbers, terminated by a 0:");
-        while(sc.hasNextInt()){
+        while (sc.hasNextInt()) {
             number = sc.nextInt();
-            if (number > 0 || number < 0){
-                multiplication = number*multiplication;
-            times++;
-            }else{
-                System.out.println("Multiplication is "+ multiplication);
-                System.out.println("The amount of variables entered is "+times);
+            if (number > 0 || number < 0) {
+                multiplication = number * multiplication;
+                times++;
+            } else {
+                System.out.println("Multiplication is " + multiplication);
+                System.out.println("The amount of variables entered is " + times);
                 break;
             }
         }
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         (new Summation()).doSummation();
     }
 }
