4ae7062cdc7439d29b36467521ef60177ffb6d6b
Ades-Angel <64943306+Ades-Angel@users.noreply.github.com>
2021-09-23 21:34:23 +0200

    Update Expressions.java

--- Exercise/Set 1/Expressions.java
+++ Exercise/Set 1/Expressions.java
@@ -7,9 +7,10 @@ Calculate an expression and output it
 
 public class Expressions {
     void show() {
-        System.out.println(13*13);
+        System.out.println(13 * 13);
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         (new Expressions()).show();
     }
 }
