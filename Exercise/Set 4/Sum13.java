6435fd6dc0a8e10b56ac3dd71103c0d02aa9c732
Chloe Sandra Antonozzi <64943306+Ades-Angel@users.noreply.github.com>
2021-10-05 22:58:19 +0200

    Create Sum13.java

---
+++ Exercise/Set 4/Sum13.java
@@ -0,0 +1,19 @@
+/*
+Chloe Antonozzi
+1670980
+05/10/2021
+
+Coding Bat
+*/
+public int sum13(int[] nums) {
+  int sum = 0;
+  for(int i=0; i<nums.length; i++){
+    if (nums[i] != 13){
+      sum += nums[i];
+    }else if(nums[i] == 13 && i<nums.length - 1){
+      nums[i] = 0;
+      nums[i+1]=0;
+    }
+  }
+  return sum;
+}
