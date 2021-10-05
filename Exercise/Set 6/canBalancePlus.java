3f97ffa5ea7d625e0d8bb9c55d36e07f2fbe0c09
Chloe Sandra Antonozzi <64943306+Ades-Angel@users.noreply.github.com>
2021-10-05 22:43:21 +0200

    Create canBalancePlus.java

---
+++ Exercise/Set 6/canBalancePlus.java
@@ -0,0 +1,23 @@
+/*
+Chloe Antonozzi
+1670980
+05/20/2021
+
+Coding Bat
+*/
+boolean canBalancePlus(int[] nums) {
+  int lSum = 0;
+  
+  for (int i=0; i<nums.length; i++){
+    lSum += nums[i];
+    int rSum = 0;
+    
+    for (int j = nums.length -1; j >i; j--){
+      rSum += nums[j];
+    }
+    if (lSum == rSum){
+      return true;
+    }
+  }
+  return false;
+}
