7a5988abce8ab9c74a7a06fb7d78d1711de0e795
Chloe Sandra Antonozzi <64943306+Ades-Angel@users.noreply.github.com>
2021-10-05 22:39:39 +0200

    Create nestParen.java

---
+++ Exercise/Set 6/nestParen.java
@@ -0,0 +1,17 @@
+/*
+Chloe Antonozzi
+1670980
+05/10/2021
+
+Coding Bat
+*/
+
+public boolean nestParen(String str) {
+    if(str.length() == 0)
+        return true;
+          
+    if(str.charAt(0) == '(' && str.charAt(str.length()-1) == ')')
+        return nestParen(str.substring(1, str.length()-1));
+                    
+    return false;
+}
