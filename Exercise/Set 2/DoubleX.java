ededc24595519e5d5e62f6bc550fe421ca751b21
Chloe Sandra Antonozzi <64943306+Ades-Angel@users.noreply.github.com>
2021-10-05 23:09:00 +0200

    Create DoubleX.java

---
+++ Exercise/Set 2/DoubleX.java
@@ -0,0 +1,13 @@
+/*
+Chloe Antonozzi
+1670980
+05/10/2021
+
+Coding Bat
+*/
+boolean doubleX(String str) {
+  int i = str.indexOf("x");
+  if (i == -1) return false;
+  if (i+1 >= str.length()) return false;
+  return str.substring(i+1, i+2).equals("x");
+}
