96c51b813ca24de9af0d8215e836af426060b9a6
Chloe Sandra Antonozzi <64943306+Ades-Angel@users.noreply.github.com>
2021-10-05 23:00:34 +0200

    Create ArrayFront9.java

---
+++ Exercise/Set 4/ArrayFront9.java
@@ -0,0 +1,19 @@
+/*
+Chloe Antonozzi
+1670980
+05/10/2021
+
+Coding Bat
+*/
+public boolean arrayFront9(int[] nums) {
+  int end = nums.length;
+  if (end>4){
+    end = 4;
+  }
+  for(int i=0; i<end; i++){
+    if(nums[i] == 9){
+      return true;
+    }
+  }
+   return false;
+}
