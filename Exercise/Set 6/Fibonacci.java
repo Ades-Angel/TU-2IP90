1fc4abddc6c1593131d61d92f65a52cc97dc549d
Chloe Sandra Antonozzi <64943306+Ades-Angel@users.noreply.github.com>
2021-10-05 22:41:11 +0200

    Create Fibonacci.java

---
+++ Exercise/Set 6/Fibonacci.java
@@ -0,0 +1,14 @@
+/*
+Chloe Antonozzi
+1670980
+05/10/2021
+
+Coding Bat
+*/
+public int fibonacci(int n) {
+  if (n <= 1){
+    return n;
+  }else{
+    return fibonacci(n -1) + fibonacci(n -2);
+  }
+}
