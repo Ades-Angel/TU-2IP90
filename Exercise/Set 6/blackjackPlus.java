22c5a6cd2e0b356fae7a98b9cf7f739cc333f55c
Chloe Sandra Antonozzi <64943306+Ades-Angel@users.noreply.github.com>
2021-10-05 22:44:30 +0200

    Create blackjackPlus.java

---
+++ Exercise/Set 6/blackjackPlus.java
@@ -0,0 +1,30 @@
+/*
+Chloe Antonozzi
+1670980
+05/20/2021
+
+Coding Bat
+*/
+public int blackjackPlus(int a, int b) { 
+  int aa = bj(a);
+  int bb = bj(b);
+  int r = max( aa, bb);
+  
+  return r;
+}
+
+int max(int a, int b){
+  if (a > b){
+    return a;
+  }else{
+    return b;
+  }
+}
+
+int bj(int n){
+  if(n <=21){
+    return n;
+  }else{
+    return 0;
+  }
+}
