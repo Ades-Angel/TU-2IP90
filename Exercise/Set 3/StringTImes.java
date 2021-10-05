5617b013150fc08a3e00b92bab5a2a148bb9cb41
Chloe Sandra Antonozzi <64943306+Ades-Angel@users.noreply.github.com>
2021-10-05 22:50:27 +0200

    Create StringTImes.java

---
+++ Exercise/Set 3/StringTImes.java
@@ -0,0 +1,14 @@
+/*
+Chloe Antonozzi
+1670980
+05/20/2021
+
+Coding Bat
+*/
+public String stringTimes(String str, int n) {
+  String result = "";
+  for (int i=0; i<n; i++){
+    result = result + str;
+  }
+  return result;
+}
