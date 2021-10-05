f78793664b52628fe6235da0e47e41b9b3ee823e
Chloe Sandra Antonozzi <64943306+Ades-Angel@users.noreply.github.com>
2021-10-05 23:11:45 +0200

    Create MiddleTwo.java

---
+++ Exercise/Set 2/MiddleTwo.java
@@ -0,0 +1,20 @@
+/*
+Chloe Antonozzi
+1670980
+05/10/2021
+
+Coding Bat
+*/
+public String middleTwo(String str) {
+  int position;
+  int length;
+  
+  if (str.length() % 2 == 0){
+    position = str.length()/2 -1;
+    length = 2;
+  }else{
+    position = str.length()/2;
+    length = 1;
+  }
+  return str.substring(position, position + length);
+}
