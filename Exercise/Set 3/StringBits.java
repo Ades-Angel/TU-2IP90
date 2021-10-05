e505aff6f56bc4baabb5e5f0ad52ab02aecb8019
Chloe Sandra Antonozzi <64943306+Ades-Angel@users.noreply.github.com>
2021-10-05 22:52:52 +0200

    Create StringBits.java

---
+++ Exercise/Set 3/StringBits.java
@@ -0,0 +1,14 @@
+/*
+Chloe Antonozzi
+1670980
+05/10/2021
+
+Coding Bat
+*/
+public String stringBits(String str) {
+  String result = "";
+  for (int i = 0; i<str.length(); i +=2){
+    result = result + str.substring(i, i+1);
+  }
+  return result;
+}
