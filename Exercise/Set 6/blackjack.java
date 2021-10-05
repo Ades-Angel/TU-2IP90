4d34131fb633fbaa2184769181ff33b1b925d5de
Chloe Sandra Antonozzi <64943306+Ades-Angel@users.noreply.github.com>
2021-10-05 22:45:38 +0200

    Create blackjack.java

---
+++ Exercise/Set 6/blackjack.java
@@ -0,0 +1,22 @@
+/*
+Chloe Antonozzi
+1670980
+05/10/2021
+
+Coding Bat
+*/
+public int blackjack(int a, int b) {
+  if (a <=21 && b <=21){
+    if (a > b){
+      return a;
+    }else{
+      return b;
+    }
+  }else if( a <= 21){
+    return a;
+  }else if(b <=21){
+    return b;
+  }else{
+    return 0;
+  }
+}
