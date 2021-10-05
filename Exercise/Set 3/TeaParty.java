cf362353439824063d9742eaace3176ba74f8a2e
Chloe Sandra Antonozzi <64943306+Ades-Angel@users.noreply.github.com>
2021-10-05 23:12:41 +0200

    Create TeaParty.java

---
+++ Exercise/Set 3/TeaParty.java
@@ -0,0 +1,20 @@
+/*
+Chloe Antonozzi
+1670980
+05/10/2021
+
+Coding Bat
+*/
+public int teaParty(int tea, int candy) {
+  if (tea+tea <= candy || candy+candy <= tea){
+    if (tea>=5 && candy>=5){
+      return 2;
+    }else{
+      return 0;
+    }
+  }else if (tea>=5 && candy>=5){
+    return 1;
+  }else{
+    return 0;
+  }
+}
