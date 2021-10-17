81e70dbbd4c5600e5bbb5442f0c83bdb16595a53
Chloe Sandra Antonozzi <64943306+Ades-Angel@users.noreply.github.com>
2021-10-17 14:12:01 +0200

    Update SomeComponents.java

--- Exercise/Set 9/SomeComponents.java
+++ Exercise/Set 9/SomeComponents.java
@@ -1,3 +1,4 @@
+
 /*
 Chloe Antonozzi
 1670980
@@ -15,12 +16,12 @@ class SomeComponents {
     JTextField text;
     ImageIcon image;
 
-    SomeComponents(){
-        frame = new JFrame("Some Components");      // creates frame
+    SomeComponents() {
+        frame = new JFrame("Some Components"); // creates frame
         lbl = new JLabel("What's \n my \n color?");
-        button = new JButton("Ok");                 // creates component: JButton
+        button = new JButton("Ok"); // creates component: JButton
         button2 = new JButton("Not Ok");
-        panel = new JPanel();                       // creates another component
+        panel = new JPanel(); // creates another component
         text = new JTextField("Don't press these buttons");
         image = new ImageIcon("cat.jpg");
         icon = new JLabel(image);
@@ -28,22 +29,22 @@ class SomeComponents {
 
     }
 
-    void buildIt(){
-        frame.add(panel);                           // places panel in center of frame
-        
+    void buildIt() {
+        frame.add(panel); // places panel in center of frame
+
         lbl.setBackground(Color.PINK);
         lbl.setOpaque(true);
 
-        frame.add(button, BorderLayout.SOUTH);      // places button at bottom of frame
+        frame.add(button, BorderLayout.SOUTH); // places button at bottom of frame
         frame.add(button2, BorderLayout.WEST);
 
-        Color color = new Color(255, 255, 0);       // makes helper object: color yellow
-        panel.setBackground(color);                 // color background panel
+        Color color = new Color(255, 255, 0); // makes helper object: color yellow
+        panel.setBackground(color); // color background panel
 
         frame.add(lbl, BorderLayout.NORTH);
         frame.add(text, BorderLayout.NORTH);
 
-        frame.add(icon, BorderLayout.EAST);
+        frame.add(icon, BorderLayout.CENTER);
 
         frame.setSize(400, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -51,7 +52,7 @@ class SomeComponents {
         frame.setVisible(true);
     }
 
-    public static void main (String[] args){
+    public static void main(String[] args) {
         SomeComponents sc = new SomeComponents();
         sc.buildIt();
     }
