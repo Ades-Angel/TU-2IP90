2257a944ee234bc6f88894537c38c05a74ffd099
Chloe Sandra Antonozzi <64943306+Ades-Angel@users.noreply.github.com>
2021-10-18 13:04:38 +0200

    Update Vehicle.java

--- Exercise/Set 10/Vehicle.java
+++ Exercise/Set 10/Vehicle.java
@@ -9,63 +9,63 @@ Chloe Antonozzi
 
 class Vehicle {
 
-    int registrationNumber; 
-    int weight;
+  int registrationNumber;
+  int weight;
 
-    public Vehicle(int registrationNumber, int weight) {
-      this.registrationNumber = registrationNumber;
-      this.weight = weight;
-    }
-
-    @Override
-    public String toString() {
-        return "Vehicle with reg.nr. : " + registrationNumber + "\nWeight: " + weight;
-    }
-    
-  }
-  
-  class Car extends Vehicle {
-
-    private String vehicleType;
-    private String fuelType;
-  
-    public Car(int registrationNumber,int weight, String vehicleType, String fuelType) {
-      super(registrationNumber, weight);
-      this.vehicleType = vehicleType;
-      this.fuelType = fuelType;
-    }
-  
-    public String toString() {
-        return super.toString() + "\nVehicle Type: " + vehicleType + "\nFuel Type: " + fuelType;
-    }
+  public Vehicle(int registrationNumber, int weight) {
+    this.registrationNumber = registrationNumber;
+    this.weight = weight;
   }
 
-  class Motorcycle extends Vehicle {
-
-    private String hasSidecar;
-    private String vehicleType;
-
-    public Motorcycle(int registrationNumber, int weight, String vehicleType, String hasSidecar) {
-      super(registrationNumber, weight);
-      this.vehicleType = vehicleType;
-      this.hasSidecar = hasSidecar;
-    }
-  
-    public String toString() {
-        return super.toString() + "\nVehicle Type: " + vehicleType + "\nSidecar: " + hasSidecar;
-    }
+  @Override
+  public String toString() {
+    return "Vehicle with reg.nr. : " + registrationNumber + "\nWeight: " + weight;
   }
 
-  class Truck extends Vehicle {
+}
 
-    private String vehicleType;
+class Car extends Vehicle {
 
-    public Truck(int registrationNumber, int weight, String vehicleType) {
-      super(registrationNumber, weight);
-      this.vehicleType = vehicleType;
-    }
-  
-    public String toString() {
-        return super.toString() + "\nVehicle Type: " + vehicleType;
-    }
+  private String vehicleType;
+  private String fuelType;
+
+  public Car(int registrationNumber, int weight, String vehicleType, String fuelType) {
+    super(registrationNumber, weight);
+    this.vehicleType = vehicleType;
+    this.fuelType = fuelType;
   }
+
+  public String toString() {
+    return super.toString() + "\nVehicle Type: " + vehicleType + "\nFuel Type: " + fuelType;
+  }
+}
+
+class Motorcycle extends Vehicle {
+
+  private String hasSidecar;
+  private String vehicleType;
+
+  public Motorcycle(int registrationNumber, int weight, String vehicleType, String hasSidecar) {
+    super(registrationNumber, weight);
+    this.vehicleType = vehicleType;
+    this.hasSidecar = hasSidecar;
+  }
+
+  public String toString() {
+    return super.toString() + "\nVehicle Type: " + vehicleType + "\nSidecar: " + hasSidecar;
+  }
+}
+
+class Truck extends Vehicle {
+
+  private String vehicleType;
+
+  public Truck(int registrationNumber, int weight, String vehicleType) {
+    super(registrationNumber, weight);
+    this.vehicleType = vehicleType;
+  }
+
+  public String toString() {
+    return super.toString() + "\nVehicle Type: " + vehicleType;
+  }
+}
