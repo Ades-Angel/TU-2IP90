d5ef6cebb475d69680490ac904130e2419c0fb49
Chloe Sandra Antonozzi <64943306+Ades-Angel@users.noreply.github.com>
2021-10-19 12:30:49 +0200

    Add files via upload

---
+++ Exercise/Set 4/EchoPit.java
@@ -0,0 +1,28 @@
+package Homework.Set4;
+
+/*
+Chloe Antonozzi
+16/09/2021
+
+19/10/2021
+*/
+import java.util.*;
+
+public class EchoPit {
+    Scanner scan = new Scanner(System.in);
+
+    public String reverse(String input) {
+        List<String> list = Arrays.asList(input.split(" "));
+        Collections.reverse(list);
+        return String.join(" ", list);
+    }
+
+    public void echo() {
+        String input = scan.nextLine();
+        System.out.println(reverse(input));
+    }
+
+    public static void main(String[] args) {
+        (new EchoPit()).echo();
+    }
+}
