bacf1ea70eb7bb8078105e31c7366b1fe7a0539e
Chloe Sandra Antonozzi <64943306+Ades-Angel@users.noreply.github.com>
2021-11-01 11:37:25 +0100

    Update Without2.java

--- Exercise/Set 2/Without2.java
+++ Exercise/Set 2/Without2.java
@@ -5,6 +5,7 @@ Chloe Antonozzi
 05/10/2021
 Coding Bat
 */
+
 public String without2(String str) {
     if(str.length () >= 2 && 
         str.substring(0, 2).equals(str.substring(str.length() - 2)))
